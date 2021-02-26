1a9a105186a92d8a4cfe6825d266909cc08bd835
Alexander Vikhrov <vih_64@mail.ru>
2021-02-26 16:55:24 +0400

    Added the job 1.1.9

---
+++ Task 1/1.1.9. Non-Negative Sum/1.1.9. Non-Negative Sum/NonNegativeSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._1._9._Non_Negative_Sum
+{
+    class NonNegativeSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** NonNegativeSum ***");
+            getNonNegativeSum();
+        }
+
+        static void getNonNegativeSum()
+        {
+            int[] mass = { 1, 2, 3, -4, -5, 10 };
+
+            int Sum=0;
+            for(int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] < 0)
+                {
+                    mass[i] = 0;
+                }
+
+                Console.WriteLine($"Sum = {Sum += mass[i]}");
+            }
+        }
+    }
+}
