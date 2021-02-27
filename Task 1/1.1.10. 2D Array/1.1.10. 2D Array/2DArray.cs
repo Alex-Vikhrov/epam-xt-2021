97385c1cd9d17bb59c2f523a189eb875940221bb
Alexander Vikhrov <vih_64@mail.ru>
2021-02-27 16:03:00 +0400

    completed 1.1.10

--- Task 1/1.1.10. 2D Array/1.1.10. 2D Array/2DArray.cs
+++ Task 1/1.1.10. 2D Array/1.1.10. 2D Array/2DArray.cs
@@ -2,45 +2,38 @@
 
 namespace _1._1._10._2D_Array
 {
-    class Program
+    class Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("[a] + [b] % 2");
+            Console.WriteLine("*** 2DArray ***");
             getSum2DArray();
         }
 
         static void getSum2DArray()
         {
             //Console.WriteLine("Введите размерность массива:");
+            //Console.WriteLine("Введите размерность 1:");
             //int a = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите размерность 2:");
             //int b = Int32.Parse(Console.ReadLine());
             //Console.WriteLine();
 
-            int[,] mass = new int[5, 5];
-            Random r = new Random();
+            int[,] mass = new int[3, 2]; 
 
+            int x = 0;
             int sum = 0;
             for (int i = 0; i < mass.GetLength(0); i++)
             {
                 for (int j = 0; j < mass.GetLength(1); j++)
                 {
+                    mass[i, j] = x++;
                     if ((i + j) % 2 == 0)
                     {
                         Console.WriteLine($"Sum = {sum += mass[i, j]}");
                     }
                 }
             }
-
-            for (int i = 0; i < mass.GetLength(0); i++)
-            {
-                for (int j = 0; j < mass.GetLength(1); j++)
-                {
-                    mass[i, j] = r.Next(-20, 20);
-                }
-                Console.WriteLine();
-            }
-            
         }
     }
 }
