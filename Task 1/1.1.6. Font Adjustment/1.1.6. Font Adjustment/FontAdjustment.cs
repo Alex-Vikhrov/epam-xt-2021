c6ad244750d79e69a4a416235717eabf5ef04f5a
Alexander Vikhrov <vih_64@mail.ru>
2021-02-27 16:04:26 +0400

    Made 1.1.6 and completed 1.1.7

---
+++ Task 1/1.1.6. Font Adjustment/1.1.6. Font Adjustment/FontAdjustment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._1._6._Font_Adjustment
+{
+    class FontAdjustment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Font Adjustment ***");
+            getFontAdjustment();
+        }
+
+        static void getFontAdjustment()
+        {
+            List<string> Font = new List<string>();
+            int numberFont;
+            do
+            {
+                Console.Write("Введите:" + "\n" +
+                    "1: bold" + "\n" +
+                    "2: italic" + "\n" +
+                    "3: underline" + "\n" +
+                    "4: cancel" + "\n" +
+                    "5: exit");
+                Console.WriteLine();
+                // Проверка на число 
+                if (!Int32.TryParse(Console.ReadLine(), out numberFont))
+                {
+                    Console.WriteLine("Введите число!");
+                }
+                
+                switch (numberFont)
+                {
+                    case 1:
+                        if (!Font.Contains("Bold"))
+                        {
+                            Font.Add("Bold");
+                        }
+                        else
+                        {
+                            Font.Remove("Bold");
+                        }
+                        Console.WriteLine("Параметры надписи:" + string.Join(", ", Font));
+                        break;
+
+                    case 2:
+                        if (!Font.Contains("Italic"))
+                        {
+                            Font.Add("Italic");
+                        }
+                        else
+                        {
+                            Font.Remove("Italic");
+                        }
+                        Console.WriteLine("Параметры надписи:" + string.Join(", ", Font));
+                        break;
+
+                    case 3:
+                        if (!Font.Contains("Underline"))
+                        {
+                            Font.Add("Underline");
+                        }
+                        else
+                        {
+                            Font.Remove("Underline");
+                        }
+                        Console.WriteLine("Параметры надписи:" + string.Join(", ", Font));
+                        break;
+                        // кейс 4 - не могу понять почему он не удаляет их всех 
+                    case 4:
+                        if (!Font.Contains("Параметры надписи:"))
+                        {
+                            Font.Remove("Bold");
+                            Font.Remove("Underline");
+                            Font.Remove("Underline");
+                        }
+                        Console.WriteLine("Параметры надписи:");
+                        break;
+
+                    case 5:
+                        break;
+                }
+            } while (numberFont < 5);
+        }
+    }
+}
---
+++ Task 1/1.1.7. Array Processing/1.1.7. Array Processing/ArrayProcessing.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _1._1._7._Array_Processing
+{
+    class ArrayProcessing
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Array Processing ***");
+            getArrayProcessing();
+        }
+
+        static void getArrayProcessing()
+        {
+            // Ввод чисел массива
+            int x;
+            Console.Write("Введите числа в массив :");
+            x = int.Parse(Console.ReadLine());
+            int[] numbers = new int[x];
+
+            // Заполнение массива рандомными числами
+            Random r = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(100);
+            }
+
+            // Сортировка рандомного массива
+            int sort;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        sort = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = sort;
+                    }
+                }
+            }
+            // Вывод отсортированного массива 
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Сравнение максимального и минимального элемента в массиве 
+            int max = numbers[0]; 
+            int min = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if(max < num)
+                {
+                    max = num;
+                }
+                else if (min > num) 
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine("Min:{0}", min);
+            Console.WriteLine("Max:{0}", max);
+        }
+    }
+}
