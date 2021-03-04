d444c159a3f4e45005b5edab1f2aad27d70e3a91
Alexander Vikhrov <vih_64@mail.ru>
2021-03-04 20:40:22 +0400

    Added tasks 1.2.1 and 1.2.3

---
+++ Task 1/1.2/1.2.1. AVERAGES/1.2.1. AVERAGES/Avegares.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _1._2._1._AVERAGES
+{
+    class Avegares
+    {
+        static void Main(string[] args)
+        {
+            GetStrings();
+            Console.WriteLine();
+        }
+        static void GetStrings()
+        {
+            Console.WriteLine(" Введите слово: ");
+            string N = Console.ReadLine();
+            StringBuilder word = new StringBuilder();
+            int f = 0;
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                if (char.IsLetterOrDigit(N[i]))
+                    f++;//счетчик записывающий все символы
+                if (!char.IsPunctuation(N[i]))
+                {
+                    word.Append(N[i]);// входят только буквы и цифры
+                }
+            }
+            int g = word.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries).Length; // массив из количеста слов
+            Console.WriteLine(" Средняя длина вашего слова равна :" + (f / g));
+        }
+    }
+}
---
+++ Task 1/1.2/1.2.3. LOWERCASE/1.2.3. LOWERCASE/LOWERCASE.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace _1._2._3._LOWERCASE
+{
+    class LOWERCASE
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** LOWERCASE ***");
+            calcLowercase();
+        }
+
+        static void calcLowercase()
+        {
+            Console.WriteLine(" Введите предложение: ");
+            string text = Console.ReadLine();
+            int g = 0;
+            char [] symbol = new char[] { ' ', ',', '.' };
+            string [] words = text.Split(symbol, StringSplitOptions.RemoveEmptyEntries);// words Записывает слова
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char firstChar = words[i][0]; // Смотрит первый символ
+                if (char.IsLower(firstChar))
+                {
+                    g++;
+                }
+            }
+            Console.WriteLine("Слова написанные с маленькой буквы: " + (g));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//if (char.IsLetterOrDigit(text[i]))
+//    f++;
+//if (!char.IsPunctuation(text[i]) && char.IsLower(text[i]))
+//{
+//    word.Append(text[i]);
+//}
+////if (char.IsUpper(text[i]))// проверку на верхний регистр 
+////{
+////    word.Remove(text)
+////}
