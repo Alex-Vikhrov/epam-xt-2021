67095b86b79197781a9900843b99595208bb9f27
Alexander Vikhrov <vih_64@mail.ru>
2021-03-20 16:48:57 +0400

    Added a concatenation method and a character comparison method

---
+++ Task 2/2.1.1. Custom String - v2/2.1.1. Custom String - v2/MyStringBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2._1._1._Custom_String___v2
+{
+    class MyStringBuilder
+    {
+        public char[] symbol;
+
+        // Метод конкатенации символов: используем конструктор
+        public MyStringBuilder(string value)
+        {
+            this.symbol = value.ToCharArray();
+        }
+        public void Concatenate(string text) 
+        {
+            char[] temp = new char[symbol.Length + text.Length];
+
+            for (int i = 0; i < symbol.Length; i++)
+            {
+                temp[i] = symbol[i];
+            }
+            int index = 0;
+            for (int y = symbol.Length; y < temp.Length; y++)
+            {
+                temp[y] = text[index];
+                index++;
+            }
+            symbol = temp;
+        }
+        // Метод сравнения символов
+        public bool Comparison(char [] result)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (symbol[i] != result [i])
+                {
+                    return false;
+                }  
+            }
+            return true;
+        }
+        // Метод поиска символов 
+        public void SearchChar(char txt)
+        {
+            
+        }
+        // Метод конвертации из/в массив символов
+        public void ConversionChar()
+        {
+
+        }
+
+        public void PrintResult()
+        {
+            foreach(char a in symbol)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ Task 2/2.1.1. Custom String - v2/2.1.1. Custom String - v2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+namespace _2._1._1._Custom_String___v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** My StringBuilder App ***");
+            MyStringBuilder strings = new MyStringBuilder("привет");
+            strings.Concatenate(" )");
+            strings.PrintResult();
+            strings.Comparison(new char[] {'п','р','и'});
+            
+        }
+    }
+}
