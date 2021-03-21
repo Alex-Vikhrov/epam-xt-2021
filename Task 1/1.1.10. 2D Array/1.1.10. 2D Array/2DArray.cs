f15008273f5dd1e168674e53a5dec9a192587ea8
Alexander Vikhrov <vih_64@mail.ru>
2021-03-21 18:06:02 +0400

    Left it

--- Task 1/1.1.10. 2D Array/1.1.10. 2D Array/2DArray.cs
+++ Task 1/1.1.10. 2D Array/1.1.10. 2D Array/2DArray.cs
@@ -12,22 +12,38 @@ namespace _1._1._10._2D_Array
 
         static void getSum2DArray()
         {
-            //Console.WriteLine("Введите размерность массива:");
-            //Console.WriteLine("Введите размерность 1:");
-            //int a = Int32.Parse(Console.ReadLine());
-            //Console.WriteLine("Введите размерность 2:");
-            //int b = Int32.Parse(Console.ReadLine());
-            //Console.WriteLine();
+            Console.WriteLine("Введите размерность массива:");
+            Console.WriteLine("Введите размерность 1:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите размерность 2:");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
 
-            int[,] mass = new int[3, 2]; 
-
-            int x = 0;
             int sum = 0;
+            int[,] mass = new int[a, b];
             for (int i = 0; i < mass.GetLength(0); i++)
             {
                 for (int j = 0; j < mass.GetLength(1); j++)
                 {
-                    mass[i, j] = x++;
+                    Console.WriteLine($"Введите числа массива по индексу [{i} {j}]");
+                    mass[i,j] = Int32.Parse(Console.ReadLine()); //mass[i, j] = x++;
+                    //if ((i + j) % 2 == 0)
+                    //{
+                    //    Console.WriteLine($"Sum = {sum += mass[i, j]}");
+                    //}
+                }
+            }
+
+            //int[,] mass = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            //int[,] mass = new int[3, 2]; 
+
+            //int x = 0;
+            //int sum = 0;
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    //mass[i, j] = x++;
                     if ((i + j) % 2 == 0)
                     {
                         Console.WriteLine($"Sum = {sum += mass[i, j]}");
