84b30f9950d1b47889f0ae49a9db4276a6c600a9
Alexander Vikhrov <vih_64@mail.ru>
2021-04-13 17:03:21 +0400

    Initial version task 3.3.1

---
+++ Task 3/Task 3.3/3.3.1. SUPER ARRAY/3.3.1. SUPER ARRAY/Number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _3._3._1._SUPER_ARRAY
+{
+    class Number
+    {
+        public Number(int[] array)
+        {
+            this.Array = array;
+        }
+
+        public int[] Array { get; }
+
+        public int CalcSum(Func<int, int> selector)
+        {
+            return Array.Sum(selector);
+            //int sum = 0;
+            //foreach (var item in Array)
+            //{
+            //    sum += selector(item);
+            //}
+            //return sum;
+        }
+
+        public int CalcAverageValue(Func<int, int> value)
+        {
+            return Array.Sum() / Array.Length;
+        }
+
+        public void GetFrequencyValue()
+        {
+
+        }
+    }
+}
---
+++ Task 3/Task 3.3/3.3.1. SUPER ARRAY/3.3.1. SUPER ARRAY/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3._3._1._SUPER_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number(new int[] { 1, 2, 3, 4 });
+            number.CalcSum(n => n + 1);
+        }
+    }
+}
