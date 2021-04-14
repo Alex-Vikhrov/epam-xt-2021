c9cdd83b0d7235d3509d2cc55e169ede82cd5f3d
Alexander Vikhrov <vih_64@mail.ru>
2021-04-14 12:53:13 +0400

    Finished version

--- Task 3/Task 3.3/3.3.2. SUPER STRING/3.3.2. SUPER STRING/Program.cs
+++ Task 3/Task 3.3/3.3.2. SUPER STRING/3.3.2. SUPER STRING/Program.cs
@@ -24,29 +24,21 @@ namespace _3._3._2._SUPER_STRING
             Regex english = new Regex("[A-Za-z]");
             Regex russian = new Regex("[А-Яа-яё]");
             Regex number = new Regex("[0-9]");
-            Regex mixed = new Regex("[0-9A-Za-zА-яа-яё]");
 
             MatchCollection eng = english.Matches(txt);
             MatchCollection rus = russian.Matches(txt);
             MatchCollection num = number.Matches(txt);
-            MatchCollection mix = mixed.Matches(txt);
 
-            if(eng.Count == txt.Length)
+            if (eng.Count == txt.Length)
             {
                 Console.WriteLine("English");
-            }
-
-            if(rus.Count == txt.Length)
+            } else if (rus.Count == txt.Length)
             {
                 Console.WriteLine("Russian");
-            }
-
-            if (num.Count == txt.Length)
+            } else if (num.Count == txt.Length)
             {
                 Console.WriteLine("Number");
-            }
-
-            if (mix.Count == txt.Length)
+            } else 
             {
                 Console.WriteLine("Mixed");
             }
